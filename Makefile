d03de25d286034a6d0b3ed76eb7abf69a5456579
Gilson Filho <me@gilsondev.in>
2022-10-11 09:38:35 -0300

    refactor: Prepare SQLAlchemy stub to mypy

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
 .PHONY: export-diagrams
 export-diagrams:
 	@docker-compose run --rm plantuml /usr/docs/assets/diagrams/*.puml
+
+.PHONY: api-test
+api-test:
+	@cd ./api && pytest	
+
+.PHONY: api-mypy
+api-mypy:
+	@cd ./api && mypy .
--- api/migrations/env.py
+++ api/migrations/env.py
@@ -63,9 +63,9 @@ def run_migrations_online() -> None:
 
     """
     configuration = config.get_section(config.config_ini_section)
-    configuration["sqlalchemy.url"] = get_url()
+    configuration["sqlalchemy.url"] = get_url()  # type: ignore
     connectable = engine_from_config(
-        configuration,
+        configuration,  # type: ignore
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
