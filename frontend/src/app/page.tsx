97bf02c7324d704d0aad46af98babba9b385dc3d
Gilson Filho <me@gilsondev.in>
2023-09-23 15:42:25 -0300

    chore(frontend): Define project structure

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main_container}>
+      <h1>Next.js Works</h1>
+      <p>Example Test</p>
+      <a href="#">Link de Exemplo</a>
+    </main>
+  );
+}
