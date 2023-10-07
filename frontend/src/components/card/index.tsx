c88d943ff559d82998efd3dc90b700bd0ede0557
Gilson Filho <me@gilsondev.in>
2023-10-07 14:27:06 -0300

    fix(frontend): Define date locale to brazil

--- frontend/src/components/card/index.tsx
+++ frontend/src/components/card/index.tsx
@@ -32,11 +32,11 @@ const Card = ({ domain }: CardProps) => {
         </span>
         <span className="text-sm text-gray-500">
           <strong>Registrado: </strong>{" "}
-          {new Date(domain.registered_at).toLocaleDateString()}
+          {new Date(domain.registered_at).toLocaleDateString("pt-BR")}
         </span>
         <span className="text-sm text-gray-500">
           <strong>Última renovação: </strong>{" "}
-          {new Date(domain.updated_at).toLocaleDateString()}
+          {new Date(domain.updated_at).toLocaleDateString("pt-BR")}
         </span>
       </div>
     </a>
