b98f7ccbc9f526c1f3e30eefa0302d455a9ced93
Gilson Filho <me@gilsondev.in>
2023-10-07 10:28:23 -0300

    refactor(frontend): define loading

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,56 +4,13 @@ import Header from "@/components/header";
 import Domains from "@/components/domains";
 import { DomainsResponse } from "@/types";
 
-import React, { Suspense } from "react";
-import Skeleton from "./skeleton";
+import React from "react";
 
-interface HomeProps {
-  searchParams: {
-    [key: string]: string | string[] | undefined;
-  };
-}
-
-export default function Home({ searchParams }: HomeProps) {
-  const [domains, setDomains] = React.useState<DomainsResponse>({
-    items: [],
-    total: 0,
-    page: Number(process.env.NEXT_PUBLIC_PAGINATION_INITIAL_PAGE),
-    size: Number(process.env.NEXT_PUBLIC_PAGINATION_SIZE),
-    pages: 0,
-  });
-
-  React.useEffect(() => {
-    const page = searchParams.page
-      ? Number(searchParams.page)
-      : Number(process.env.NEXT_PUBLIC_PAGINATION_INITIAL_PAGE);
-
-    const fetchData = async () => {
-      const data = await getData(page);
-      setDomains(data);
-    };
-    fetchData();
-  }, [searchParams]);
-
+export default function Home() {
   return (
     <>
       <Header />
-      <Suspense fallback={<Skeleton />}>
-        <Domains {...domains} />
-      </Suspense>
+      <Domains />
     </>
   );
 }
-
-const getData = async (
-  page: number,
-  size: number = Number(process.env.NEXT_PUBLIC_PAGINATION_SIZE)
-): Promise<DomainsResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_CHECKSTATUS_API_URL}/domains?page=${page}&size=${size}`,
-    {
-      cache: "no-store",
-    }
-  );
-
-  return res.json();
-};
--- frontend/src/components/domains/index.tsx
+++ frontend/src/components/domains/index.tsx
@@ -1,56 +1,81 @@
 "use client";
 
+import Skeleton from "@/app/skeleton";
 import Card from "../card";
-import { Domain } from "@/types";
-import Link from "next/link";
+import { Domain, DomainsResponse } from "@/types";
+import React from "react";
 
-interface DomainsProps {
-  items: Domain[];
-  total: number;
-  page: number;
-  pages: number;
-}
+const Domains = () => {
+  const [isLoading, setLoading] = React.useState(true);
+  const [domains, setDomains] = React.useState<DomainsResponse>({
+    items: [],
+    total: 0,
+    page: 0,
+    size: 0,
+    pages: 0,
+  });
+  const [page, setPage] = React.useState(
+    Number(process.env.NEXT_PUBLIC_PAGINATION_INITIAL_PAGE)
+  );
 
-const Domains = ({ items, page, pages }: DomainsProps) => {
+  React.useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      const data = await getData(page);
+      setDomains(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, [page]);
+
+  if (isLoading) {
+    return <Skeleton />;
+  }
+
   return (
-    <main className="grid sm:grid-cols-2 md:grid-cols-1 gap-x-3" id="domains">
-      <div className="px-5 md:px-5 lg:px-16 flex flex-wrap justify-center gap-3">
-        {items?.map((domain) => (
-          <Card key={domain.slug} domain={domain} />
-        ))}
-      </div>
+    !isLoading && (
+      <main className="grid sm:grid-cols-2 md:grid-cols-1 gap-x-3" id="domains">
+        <div className="px-5 md:px-5 lg:px-16 flex flex-wrap justify-center gap-3">
+          {domains.items?.map((domain) => (
+            <Card key={domain.slug} domain={domain} />
+          ))}
+        </div>
 
-      <div className="flex justify-center mb-5">
-        {page > 1 && (
-          <Link
-            href={{
-              pathname: "/",
-              query: {
-                page: page > 1 ? page - 1 : 1,
-              },
-            }}
+        <div className="flex justify-center mb-5">
+          <button
+            disabled={page === 1}
+            onClick={() => setPage(page - 1)}
             className="flex items-center justify-center px-4 h-10 text-base font-medium text-blue-950 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700"
           >
             Anterior
-          </Link>
-        )}
+          </button>
 
-        {page < pages && (
-          <Link
-            href={{
-              pathname: "/",
-              query: {
-                page: page + 1,
-              },
-            }}
+          <button
+            onClick={() => setPage(page + 1)}
+            disabled={page === domains.pages}
             className="flex items-center justify-center px-4 h-10 ml-3 text-base font-medium text-blue-950 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700"
           >
             Próximo
-          </Link>
-        )}
-      </div>
-    </main>
+          </button>
+        </div>
+      </main>
+    )
   );
+};
+
+const getData = async (
+  page: number,
+  size: number = Number(process.env.NEXT_PUBLIC_PAGINATION_SIZE)
+): Promise<DomainsResponse> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_CHECKSTATUS_API_URL}/domains?page=${page}&size=${size}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  return res.json();
 };
 
 export default Domains;
