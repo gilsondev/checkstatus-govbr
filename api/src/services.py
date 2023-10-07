960ada30394331006ba82674da3b32531c4ff572
Gilson Filho <me@gilsondev.in>
2023-10-07 11:34:12 -0300

    feat(api): Include search domains (#20)
    * feat(api): Include search domains
    
    * refactor(api): Define type search with optional

--- api/src/services.py
+++ api/src/services.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from sqlalchemy.orm import Query
 from sqlalchemy.orm import Session
 from src.models import Domain
@@ -7,5 +9,13 @@ class DomainService:
     def __init__(self, db: Session) -> None:
         self.db = db
 
-    def fetch(self) -> Query:
-        return self.db.query(Domain)
+    def fetch(self, search: Optional[str] = "") -> Query:
+        query = self.db.query(Domain)
+
+        if not search:
+            return query
+
+        return query.filter(
+            (Domain.domain.ilike(f"%{search}%"))
+            | (Domain.organization.ilike(f"%{search}%"))
+        )
