7c51802e0840f7d3c4398c7b4f54ee9836e75b67
Gilson Filho <me@gilsondev.in>
2023-10-07 11:31:11 -0300

    feat(frontend): create api route

--- frontend/src/components/inputsearch/index.tsx
+++ frontend/src/components/inputsearch/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import React from "react";
 
 const InputSearch = () => {
@@ -37,18 +38,24 @@ const InputSearch = () => {
           </div>
           <input
             type="search"
+            name="search"
             ref={searchInputRef}
             id="default-search"
             className="block w-full p-4 pl-10 text-md text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
             placeholder="Pesquisar por domínio"
             required
           />
-          <button
-            type="submit"
+          <Link
+            href={{
+              pathname: "/domains",
+              query: {
+                search: searchInputRef.current?.value,
+              },
+            }}
             className="hidden md:block text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
           >
             Buscar
-          </button>
+          </Link>
         </div>
       </form>
     </div>
