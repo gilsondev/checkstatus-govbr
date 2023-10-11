936aeb0032ee762e40c88759f00d1a53ec06d778
Gilson Filho <me@gilsondev.in>
2023-10-11 18:53:36 -0300

    fix(scheduler): define retry policy when fetch cancelled domains (#41)

--- scheduler/src/cancelled_domains.py
+++ scheduler/src/cancelled_domains.py
@@ -1,20 +1,25 @@
-import os
+from urllib.error import URLError
 
 import pandas as pd
 from loguru import logger
 from psycopg2.extensions import cursor
+from tenacity import retry
+from tenacity import stop_after_attempt
+from tenacity import wait_fixed
 
 from lib.database import update_domain_status
 
-CANCELLED_DOMAINS_CSV = os.getenv(
-    "CANCELLED_DOMAINS_CSV",
-    "https://www.gov.br/governodigital/pt-br/transformacao-digital/ferramentas/unificacao-de-canais/lista-de-dominios-cancelados.csv",  # noqa
-)
+CANCELLED_DOMAINS_CSV = "https://www.gov.br/governodigital/pt-br/transformacao-digital/ferramentas/unificacao-de-canais/lista-de-dominios-cancelados.csv"  # noqa
 
 
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(5))
 def get_cancelled_domains() -> pd.DataFrame:
     logger.info("Getting cancelled domains...")
-    return pd.read_csv(CANCELLED_DOMAINS_CSV, names=["domain"])
+    try:
+        return pd.read_csv(CANCELLED_DOMAINS_CSV, names=["domain"])
+    except URLError as e:
+        logger.warning(f"Error while fetching cancelled domains: {e}")
+        raise ConnectionResetError("Connection reset by peer") from e
 
 
 def update_domains(cursor: cursor) -> None:
