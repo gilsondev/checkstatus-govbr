50a7e344b523ca0023fe4e072432a6fa1fd549d5
Gilson Filho <me@gilsondev.in>
2023-10-11 16:31:09 -0300

    chore(scheduler): include sentry agent

--- scheduler/src/main.py
+++ scheduler/src/main.py
@@ -1,10 +1,21 @@
 import argparse
 
+import sentry_sdk
 from cancelled_domains import update_domains as update_cancelled_domains
 from ping import ping_domains
+from sentry_sdk import set_tag
 
 from lib.database import create_connection
 from lib.database import create_cursor
+
+sentry_sdk.init(
+    dsn="https://011f53de8eea0a53cb44e163d0453eb5@o4506032925900800.ingest.sentry.io/4506032926031872",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
+set_tag("app", "Checkstatus Scheduler")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Executes tasks for checkstatus-govbr")
