624dc5c30fe301511e86071d4ddb4689397fe01f
Gilson Filho <me@gilsondev.in>
2023-10-11 16:53:11 -0300

    chore(scheduler): include sentry agent (#34)
    * chore(scheduler): include sentry agent
    
    * test(scheduler): improve test coverage

--- scheduler/tests/test_cancelled_domains.py
+++ scheduler/tests/test_cancelled_domains.py
@@ -1,6 +1,8 @@
 from unittest.mock import MagicMock
 from unittest.mock import patch
 
+from src.cancelled_domains import CANCELLED_DOMAINS_CSV
+from src.cancelled_domains import get_cancelled_domains
 from src.cancelled_domains import update_domains
 
 
@@ -17,3 +19,22 @@ def test_update_domains(mock_update_domain_status, mock_get_cancelled_domains):
     mock_update_domain_status.assert_called_with(
         mock_df.to_dict(orient="records"), mock_cursor
     )
+
+
+def test_get_cancelled_domains():
+    # Create a mock logger
+    mock_logger = MagicMock()
+
+    # Patch the logger and the pd.read_csv function
+    with patch("src.cancelled_domains.logger", mock_logger), patch(
+        "src.cancelled_domains.pd.read_csv"
+    ) as mock_read_csv:
+
+        # Call the function
+        get_cancelled_domains()
+
+        # Assert that the logger was called with the correct message
+        mock_logger.info.assert_called_once_with("Getting cancelled domains...")
+
+        # Assert that pd.read_csv was called with the correct arguments
+        mock_read_csv.assert_called_once_with(CANCELLED_DOMAINS_CSV, names=["domain"])
