651101acad689d24c0235420b33ab35161e963a9
Gilson Filho <me@gilsondev.in>
2023-10-11 19:26:34 -0300

    refactor(frontend): changes in the design page (#44)

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -4,11 +4,11 @@ import InputSearch from "../inputsearch";
 
 const Header = () => {
   return (
-    <header className="flex flex-col items-center bg-white border border-gray-300 rounded-md p-5 mt-5 md:mt-20">
+    <header className="flex flex-col items-center bg-zinc-100 border border-gray-300 shadow-sm rounded-md p-5 mt-5 md:mt-20">
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-blue-950 text-center">
         Acompanhe todos os domínios públicos registrados
       </h1>
-      <p className="hidden md:block font-extralight text-blue-950 pt-3 w-2/3 md:w-3/4 text-center lg:text-xl">
+      <p className="hidden md:block font-normal text-blue-950 pt-3 w-2/3 md:w-3/4 text-center lg:text-xl">
         Tenha acesso atualizado aos dados abertos de todos os endereços .gov.br
         registrados para acesso a sites e plataformas do governo
       </p>
