080bae0dd66f9464fc306c6a338932ea126d4f86
Gilson Filho <me@gilsondev.in>
2023-10-10 22:39:32 -0300

    refactor(scheduler): Execute tasks via args

--- scheduler/src/main.py
+++ scheduler/src/main.py
@@ -1,3 +1,5 @@
+import argparse
+
 from cancelled_domains import update_domains as update_cancelled_domains
 from ping import ping_domains
 
@@ -5,11 +7,19 @@ from lib.database import create_connection
 from lib.database import create_cursor
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Executes tasks for checkstatus-govbr")
+    parser.add_argument(
+        "task", choices=["ping", "update_cancelled_domains"], help="Task to be executed"
+    )
+    args = parser.parse_args()
+
     conn = create_connection()
     cursor = create_cursor(conn)
 
-    ping_domains(cursor)
-    update_cancelled_domains(cursor)
+    if args.task == "ping":
+        ping_domains(cursor)
+    elif args.task == "update_cancelled_domains":
+        update_cancelled_domains(cursor)
 
     conn.commit()
     conn.close()
