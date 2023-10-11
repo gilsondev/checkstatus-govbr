7a39ae0283227d5437cbf7136435f4db1558517d
Gilson Filho <me@gilsondev.in>
2023-10-11 11:09:21 -0300

    feat(frontend): include open graph

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,8 +5,32 @@ import { Montserrat } from "next/font/google";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Checkstats gov.br",
-  description: "",
+  title: "Checkstatus gov.br",
+  description: `Tenha acesso atualizado aos dados abertos de todos os
+  endereços .gov.br registrados para acesso a sites e plataformas do governo`,
+  metadataBase: new URL("https://checkstatus.gov.br"),
+  openGraph: {
+    type: "website",
+    title: "Checkstatus gov.br",
+    siteName: "Checkstatus gov.br",
+    description: `Tenha acesso atualizado aos dados abertos de todos os
+    endereços .gov.br registrados para acesso a sites e plataformas do governo`,
+    images: [
+      {
+        url: "./logo.png",
+        width: 800,
+        height: 600,
+        type: "image/png",
+      },
+      {
+        url: "./logo.png",
+        width: 1800,
+        height: 1600,
+        type: "image/png",
+      },
+    ],
+    locale: "pt_BR",
+  },
 };
 
 export default function RootLayout({
