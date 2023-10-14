17c744430958575330ab887a1ed591b1a26895ed
Gilson Filho <me@gilsondev.in>
2023-10-14 17:37:06 -0300

    Improvements (#58)
    * refactor(frontend): change domain metadata
    
    * refactor(scheduler): disable https verifier

--- scheduler/src/ping.py
+++ scheduler/src/ping.py
@@ -13,7 +13,7 @@ from lib.database import insert_domain_availability
 def check_availability(domain: str) -> bool:
     logger.info("Checking domain availability...")
     try:
-        response = requests.head(f"http://{domain}", timeout=30)
+        response = requests.head(f"http://{domain}", timeout=30, verify=False)
         return response.status_code >= 200 and response.status_code < 500
     except (exceptions.ConnectionError, exceptions.Timeout):
         return False
