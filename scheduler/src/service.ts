b30297684c9ae1ced57c3f6c0d506506d9dfe69d
Gilson Filho <me@gilsondev.in>
2023-10-19 19:46:02 -0300

    fix(scheduler): resolve update domain availability (#65)

--- scheduler/src/service.ts
+++ scheduler/src/service.ts
@@ -2,6 +2,7 @@ import { createClient } from "@checkstatusgovbr/database-ts";
 import { DatabaseClient } from "@checkstatusgovbr/database-ts/src/client";
 import AvailabilityDomains from "./availability";
 import pino from "pino";
+import { Domain } from "domain";
 
 const logger = pino();
 
@@ -19,12 +20,25 @@ export default class AvailabilityDomainsService {
     this.pingClient = new AvailabilityDomains();
   }
 
+  private async fetchDomains(): Promise<DomainData[]> {
+    try {
+      logger.info("Fetching domains");
+      const result = await this.client.sql<DomainData>(
+        "SELECT domain FROM domains",
+        []
+      );
+      return result;
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      await this.client.close();
+    }
+
+    return [];
+  }
+
   async pingDomains() {
-    logger.info("Pinging domains");
-    const domains = await this.client.sql<DomainData>(
-      "SELECT domain FROM domains",
-      []
-    );
+    const domains = await this.fetchDomains();
 
     let results: DomainData[] = [];
     for (const data of domains) {
@@ -39,15 +53,27 @@ export default class AvailabilityDomainsService {
       results.push({ ...data, available: isAvailable });
     }
 
-    const updateValues = results
-      .map((data) => `('${data.domain}', ${data.available})`)
+    await this.updateDomainsStatus(results);
+  }
+
+  public async updateDomainsStatus(domains: DomainData[]) {
+    const updateValues = domains
+      .map((data) => `('${data.domain}', ${data.available}, NOW())`)
       .join(", ");
 
-    await this.client.sql<DomainData>(
-      `UPDATE domains SET available = v.available FROM (VALUES ${updateValues}) AS v(domain, available) WHERE domains.domain = v.domain`,
-      []
-    );
+    logger.info(`Updating domains`);
+    try {
+      this.client = createClient();
+      await this.client.rawSQL<DomainData>(
+        `UPDATE domains SET available = v.available, updated_at = v.updated_at FROM (VALUES ${updateValues}) AS v(domain, available, updated_at) WHERE domains.domain = v.domain`
+      );
 
-    logger.info(`Updated ${results.length} domains`);
+      logger.info(`Updated ${domains.length} domains`);
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      logger.debug("Closing connection");
+      await this.client.close();
+    }
   }
 }
